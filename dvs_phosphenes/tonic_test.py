9cacc5766aae8b8033a66fff12cfb489ca65e3e4
Chantal van Duin <C.vanDuin@student.ru.nl>
2022-02-14 23:42:29 +0100

    Changed input file folder to corrected directory and added correct tonic event transformation

--- dvs_phosphenes/tonic_test.py
+++ dvs_phosphenes/tonic_test.py
@@ -1,10 +1,12 @@
 import h5py
 import numpy as np
+import numpy.lib.recfunctions as rfn
+
 import tonic.transforms as transforms
 
-input_file = '/home/duinch/PycharmProjects/RL-mobility/Python/Experiments/CHANTAL/v2e-output/events.h5'
-# input_file = '/home/duinch/PycharmProjects/RL-mobility/Python/Experiments/CHANTAL/v2e-box-output/events.h5'
+input_file = '/home/duinch/PycharmProjects/dvs_phosphenes/demo_output/events.h5'
 
+# data file keys of h5 file : events, frame, frame_idx, frame_ts
 data_file = h5py.File(input_file, 'r')
 print(list(data_file.keys()))
 
@@ -17,21 +19,22 @@ frames = data_file['frame']
 frame_ind = data_file['frame_idx'] # frame index
 frame_ts = data_file['frame_ts'] # delta frame time
 
-N = len(events)
-print(N)
-tonic_events = np.zeros((N,4)) # tonic events have the shape (x, y, t, p)
+# transform events into desired tonic events structure
+dtype = np.dtype([("x", int), ("y", int), ("t", int), ("p", int)])
+tonic_events = np.zeros((len(events),4))
+tonic_events[:,[2,1,0,3]] = events[:,[0,1,2,3]] # tonic events have the shape (x, y, t, p)
+tonic_events =  rfn.unstructured_to_structured(tonic_events, dtype)
 
-for i in range(0, N):
-    tonic_events[i,0] = events[i, 2] # x
-    tonic_events[i,1] = events[i, 1] # y
-    tonic_events[i,2] = events[i, 0] # t
-    tonic_events[i,3] = events[i, 3] # p
-
+# get frame information from v2e framework
 width = frames.shape[1]
 height = frames.shape[2]
 sensor_size = (width, height, 2)
 
-nr_frames = 20
+# print(tonic_events.shape)
+# print(tonic_events[0])
 
-frame_transform = transforms.ToFrame(sensor_size=sensor_size, n_time_bins=nr_frames)
+n_time_bins = 20
+
+
+frame_transform = transforms.ToFrame(sensor_size=sensor_size, n_time_bins=n_time_bins)
 tonic_frames = frame_transform(tonic_events)
