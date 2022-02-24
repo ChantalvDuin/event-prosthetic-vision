125c83e4bfe9bbec3b3f3dbd3f641f9bd6fafe9f
Chantal van Duin <C.vanDuin@student.ru.nl>
2022-02-24 13:42:58 +0100

    Removed print functions in emulator

--- dvs_phosphenes/EventStream.py
+++ dvs_phosphenes/EventStream.py
@@ -1,3 +1,8 @@
+"""
+    This module contains the class and functions for representing an event stream from
+    either reading in a h5-file or converting a traditional video to an eventstream using the v2e
+    framework.
+"""
 import numpy as np
 import os
 from v2ecore.emulator import EventEmulator
@@ -7,14 +12,31 @@ import numpy.lib.recfunctions as rfn
 import h5py
 
 class EventStream(object):
+    """
+    Computes event data from DVS camera or from traditional video using v2e framework
+    ---
+    Parameters:
+        events (array) : ndarray, [x, y, t, p] where, :
+                - x is the x-location of event in pixels
+                - y is the y-location of event in pixels
+                - t is the time-stamp of event
+                - p is the polarity of the event
+        width : frame width in pixels
+        height : frame height in pixels
+    """
     def __init__(self, num_events=100000, width=304, height=240):
-        # num_spikes: number of events this instance will initially contain
-        self.events = np.rec.array(None, dtype= [("x", int), ("y", int), ("t", int), ("p", int)],
+        self.events = np.rec.array(None, dtype= [("t", int), ("y", int), ("x", int), ("p", int)],
                                    shape=num_events)
         self.width = width
         self.height = height
 
     def read_h5_file(self, input_file):
+        """
+        Reads a given hdf5 event file and stores the events in desired event representation ordering
+        ---
+        Parameter :
+            input_file : given hdf5 event file
+        """
         data_file = h5py.File(input_file, 'r')
         h5_events = data_file['events']
         self.events = convert_v2e_events_to_tonic_events(h5_events)
@@ -22,17 +44,19 @@ class EventStream(object):
         self.width = frames.shape[1]
         self.height = frames.shape[2]
 
-
     def convert_video_to_events_v2e_emulator(self, video_path, output_folder):
-
+        """
+        Computes events from a traditional video using the v2e framework
+        ---
+        Parameters :
+            video_path: path of video to be converted
+            output_folder: output folder of v2e command
+        """
         torch.set_grad_enabled(False)
         cap = cv2.VideoCapture(video_path)
 
         # num of frames
         fps = cap.get(cv2.CAP_PROP_FPS)
-        num_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        duration = num_of_frames / fps
         delta_t = 1 / fps
         current_time = 0.
 
@@ -42,7 +66,7 @@ class EventStream(object):
 
         torch_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        # Define Emulator to convert video to events
+        # define Emulator to convert video to events
         emulator = EventEmulator(
             pos_thres=0.2,
             neg_thres=0.2,
@@ -55,23 +79,16 @@ class EventStream(object):
         )
 
         new_events = None
-
         idx = 0
-        # Only Emulate the first 10 frame
         while (cap.isOpened()):
             # Capture frame-by-frame
             ret, frame = cap.read()
-            if ret is True and idx < 10:
+            if ret is True :
                 # convert it to Luma frame
                 luma_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                # print("=" * 50)
-                # print("Current Frame {} Time {}s".format(idx, current_time))
-                # print("-" * 50)
 
                 # emulate events
-                # **IMPORTANT** The unit of timestamp here is in second, a floating number
                 new_events = emulator.generate_events(luma_frame, current_time)
-
                 ### The output events is in a numpy array that has a data type of np.int32
                 ### THe shape is (N, 4), each row is one event in the format of (t, x, y, p)
                 ### The unit of timestamp here is in microseconds
@@ -79,28 +96,11 @@ class EventStream(object):
                 # update time
                 current_time += delta_t
 
-                # print event stats
-                if new_events is not None:
-                    num_events = new_events.shape[0]
-                    start_t = new_events[0, 0]
-                    end_t = new_events[-1, 0]
-                    event_time = (new_events[-1, 0] - new_events[0, 0])
-                    event_rate_kevs = (num_events / delta_t) / 1e3
-
-                    # print("Number of Events: {}\n"
-                    #       "Duration: {}s\n"
-                    #       "Start T: {:.5f}s\n"
-                    #       "End T: {:.5f}s\n"
-                    #       "Event Rate: {:.2f}KEV/s".format(
-                    #     num_events, event_time, start_t, end_t,
-                    #     event_rate_kevs))
                 idx += 1
-                # print("=" * 50)
             else:
                 break
 
         cap.release()
-
         self.events = convert_v2e_events_to_tonic_events(new_events)
 
 def convert_v2e_events_to_tonic_events(h5_events):
