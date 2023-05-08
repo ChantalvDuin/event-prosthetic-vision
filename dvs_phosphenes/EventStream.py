f519e1b34e5c6f770ebef037587c2a3efa287be6
Chantal van Duin <C.vanDuin@student.ru.nl>
2023-05-08 17:03:07 +0200

    Created and commented Event Representation

--- dvs_phosphenes/EventStream.py
+++ dvs_phosphenes/EventStream.py
@@ -24,7 +24,7 @@ class EventStream(object):
         width : frame width in pixels
         height : frame height in pixels
     """
-    def __init__(self, num_events=100000, width=304, height=240):
+    def __init__(self, num_events=100000, width=360, height=240):
         self.events = np.rec.array(None, dtype= [("t", int), ("y", int), ("x", int), ("p", int)],
                                    shape=num_events)
         self.width = width
@@ -39,7 +39,21 @@ class EventStream(object):
         """
         data_file = h5py.File(input_file, 'r')
         h5_events = data_file['events']
-        self.events = convert_v2e_events_to_tonic_events(h5_events)
+        self.events = convert_h5_events_to_tonic_events(h5_events)
+        frames = data_file['frame']
+        self.width = frames.shape[1]
+        self.height = frames.shape[2]
+
+    def read_h5_file_different(self, input_file):
+        """
+        Reads a given hdf5 event file and stores the events in desired event representation ordering
+        ---
+        Parameters :
+            input_file : given hdf5 event file
+        """
+        data_file = h5py.File(input_file, 'r')
+        h5_events = data_file['events']
+        self.events = convert_h5_events_to_tonic_events_different(h5_events)
         frames = data_file['frame']
         self.width = frames.shape[1]
         self.height = frames.shape[2]
@@ -101,13 +115,12 @@ class EventStream(object):
                 break
 
         cap.release()
-
         # convert event ordering from the v2e desired ordering of (t, x, y, p) to the tonic preferred ordering of (x, y , t, p)
-        self.events = convert_v2e_events_to_tonic_events(new_events)
+        self.events = convert_v2e_emulator_events_to_tonic_events(new_events)
 
-def convert_v2e_events_to_tonic_events(h5_events):
+def convert_h5_events_to_tonic_events(h5_events):
         """
-        Returns the events after converting the event ordering of h5 event files and v2e events (t, x, y ,p) to the event ordering
+        Returns the events after converting the event ordering of h5 event files (t, x, y ,p) to the event ordering
         preferred by tonic : (x, y, t, p)
         ---
         Parameters :
@@ -119,11 +132,61 @@ def convert_v2e_events_to_tonic_events(h5_events):
         """
         dtype = np.dtype([("x", int), ("y", int), ("t", int), ("p", int)])
         tonic_events = np.zeros((len(h5_events), 4))
-        tonic_events[:, [2, 1, 0, 3]] = h5_events # tonic events have the shape (x, y, t, p) while h5 events : (t, y , x, p)
+        # print(h5_events[0])
+        tonic_events[:, [2, 1, 0, 3]] = h5_events # tonic events have the shape (x, y, t, p) while h5 events : (t, y, x, p)
+        # print(tonic_events[0])
         tonic_events = rfn.unstructured_to_structured(tonic_events, dtype)
+        # print(tonic_events[0])
         return tonic_events
 
-def convert_video_to_events_v2e_command(video_path, output_folder, out_filename_h5, out_filename_aedat = "None", overwrite=True, skip_video_output=False, davis_output = True):
+
+def convert_h5_events_to_tonic_events_different(h5_events):
+    """
+    Returns the events after converting the event ordering of h5 event files (t, x, y ,p) to the event ordering
+    preferred by tonic : (x, y, t, p)
+    ---
+    Parameters :
+        h5_events: events as ndarray, [t, x, y, p] where,
+            - t is the time-stamp of event
+            - x is the x-location of event in pixels
+            - y is the y-location of event in pixels
+            - p is the polarity of the event
+    """
+    dtype = np.dtype([("x", int), ("y", int), ("t", int), ("p", int)])
+    tonic_events = np.zeros((len(h5_events), 4))
+    # print(h5_events[0])
+    tonic_events[:, [2, 0, 1, 3]] = h5_events  # tonic events have the shape (x, y, t, p) while h5 events : (t, x, y, p)
+    # print(tonic_events[0])
+    tonic_events = rfn.unstructured_to_structured(tonic_events, dtype)
+    # print(tonic_events[0])
+    return tonic_events
+
+
+
+def convert_v2e_emulator_events_to_tonic_events(v2e_events):
+    """
+    Returns the events after converting the event ordering of v2e event files (t, x, y ,p) from the emulator to the event ordering
+    preferred by tonic : (x, y, t, p)
+    ---
+    Parameters :
+        v2e_events: events as ndarray, [t, x, y, p] where,
+            - t is the time-stamp of event
+            - x is the x-location of event in pixels
+            - y is the y-location of event in pixels
+            - p is the polarity of the event
+    """
+    # dtype = np.dtype([("x", '<i8'), ("y", '<i8'), ("t", '<i8'), ("p", '<i8')])
+    dtype = np.dtype([("x", int), ("y", int), ("t", int), ("p", int)])
+    # uint32
+    tonic_events = np.zeros((len(v2e_events), 4))
+    # print(v2e_events[0])
+    tonic_events[:, [1, 2, 0, 3]] = v2e_events  # tonic events have the shape (x, y, t, p) while v2e events : (t, x , y, p)
+    # print(tonic_events[0])
+    tonic_events = rfn.unstructured_to_structured(tonic_events, dtype)
+    # print(tonic_events[0])
+    return tonic_events
+
+def convert_video_to_events_v2e_command(video_path, output_folder, out_video, out_filename_h5, out_filename_aedat = "None", overwrite=True, skip_video_output=False, davis_output = True):
     """
     Extracts frames from the provided video, synthesizes fake DVS events from the video
     frames from the original video frames and generates different possible v2e outputs according to the
@@ -132,6 +195,7 @@ def convert_video_to_events_v2e_command(video_path, output_folder, out_filename_
     Parameters :
         video_path: path of video to be converted
         output_folder: folder to store v2e output
+        out_video : name of video output
         out_filename_h5: output DVS events as hdf5 event database
         out_filename_aedat: output DVS events as aedat event database
         overwrite: overwrites files in existing output folder
@@ -139,7 +203,7 @@ def convert_video_to_events_v2e_command(video_path, output_folder, out_filename_
         davis_output: saves frames within hdf5 event database
     """
     dvs_exposure = "duration .033"
-    input_frame_rate = 30
+    input_frame_rate = 20
     input_slowmotion_factor = 1
     disable_slomo = True
     slomo_model = "/content/v2e/input/SuperSloMo39.ckpt"
@@ -147,7 +211,7 @@ def convert_video_to_events_v2e_command(video_path, output_folder, out_filename_
     timestamp_resolution = 0.001
     auto_timestamp_resolution = True
 
-    condition = "Noisy"
+    condition = "Clean"
     thres = 0.2
     sigma = 0.03
     cutoff_hz = 200
@@ -176,6 +240,8 @@ def convert_video_to_events_v2e_command(video_path, output_folder, out_filename_
 
 
     v2e_command += ["--unique_output_folder", "{}".format(unique_output_folder).lower()]
+
+    v2e_command += ["--dvs_vid", out_video]
 
     if davis_output:
         v2e_command += ["--davis_output"]
